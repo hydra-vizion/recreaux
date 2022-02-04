3333685c24447db1bfe190846d9d6fba4a47002b
Luqman <luqmannur.ai@gmail.com>
2022-02-04 22:17:31 +0800

    Add navigation hamburger for dr ong sim ying

---
+++ app/src/main/java/com/example/recreaux/Dashboard.java
@@ -0,0 +1,50 @@
+package com.example.recreaux;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Dashboard extends AppCompatActivity {
+    //init var
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        //assign var
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void ClickMenu(View view){
+        //Open drawer
+        hamburger_nav.openDrawer(drawerLayout);
+    }
+
+    public void ClickLogo(View view){
+        hamburger_nav.closeDrawer(drawerLayout);
+    }
+    public void ClickHome(View view){
+        //Redirect activity to home
+        hamburger_nav.redirectActivity(this,MyProfile.class);
+    }
+    public void ClickDashboard(View view){
+        recreate();
+    }
+    public void CLickAboutUs(View view){
+        hamburger_nav.redirectActivity(this,About_us.class);
+    }
+    public void ClickLogout(View view){
+        hamburger_nav.logout(this);
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        //Close drawer
+        hamburger_nav.closeDrawer(drawerLayout);
+    }
+
+}
