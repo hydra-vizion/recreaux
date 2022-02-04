5162d7b089c19535539918b22e326ab3221e8d7f
hydra <shabilimran1@gmail.com>
2022-02-04 17:26:27 +0800

    Made Search Activity can click on listview

---
+++ app/src/main/java/com/example/recreaux/CustomListAdapter.java
@@ -0,0 +1,49 @@
+package com.example.recreaux;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CustomListAdapter extends ArrayAdapter<String> {
+
+    private Context mContext;
+    private int id;
+    private List<String> items ;
+
+    public CustomListAdapter(Context context, int textViewResourceId , List<String> list )
+    {
+        super(context, textViewResourceId, list);
+        mContext = context;
+        id = textViewResourceId;
+        items = list ;
+    }
+
+    @Override
+    public View getView(int position, View v, ViewGroup parent)
+    {
+        View mView = v ;
+        if(mView == null){
+            LayoutInflater vi = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            mView = vi.inflate(id, null);
+        }
+
+        TextView text = (TextView) mView.findViewById(R.id.customListTV);
+
+        if(items.get(position) != null )
+        {
+            text.setTextColor(Color.BLACK);
+            text.setText(items.get(position));
+            text.setBackgroundColor(Color.WHITE);
+        }
+
+
+        return mView;
+    }
+
+}
--- app/src/main/java/com/example/recreaux/SearchTab.java
+++ app/src/main/java/com/example/recreaux/SearchTab.java
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.database.DataSnapshot;
@@ -23,10 +26,12 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
 
-public class SearchTab extends AppCompatActivity {
+public class SearchTab extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     TabLayout tabLayout;
     ViewPager2 pager2;
@@ -37,14 +42,19 @@ public class SearchTab extends AppCompatActivity {
     private ListView mListView;
     //vars
     private List<String> mUserList;
-
+    private int currentTabPos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_tab);
 
+
         mListView = (ListView) findViewById(R.id.searchList);
+        mListView.setOnItemClickListener(this);
+
         mSearchParam = (EditText) findViewById(R.id.searchBar);
+
+
         tabLayout = findViewById(R.id.tab_search);
         pager2  = findViewById(R.id.search_view_pager);
 
@@ -61,6 +71,9 @@ public class SearchTab extends AppCompatActivity {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 pager2.setCurrentItem(tab.getPosition());
+                currentTabPos = tab.getPosition();
+                mUserList.clear();
+                updateUserName();
             }
 
             @Override
@@ -78,38 +91,150 @@ public class SearchTab extends AppCompatActivity {
             @Override
             public void onPageSelected(int position) {
                 tabLayout.selectTab(tabLayout.getTabAt(position));
+
             }
         });
     }
     private void searchForMatch(String keyword){
         mUserList.clear();
-
-        if(keyword.length() == 0 ){
+        updateUserName();
+        if(keyword.length() <= 2 ){
 
         }else{
-            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("User");
+            DatabaseReference referenceUser = FirebaseDatabase.getInstance().getReference("User");
+            DatabaseReference referenceEvent = FirebaseDatabase.getInstance().getReference("Event");
+            DatabaseReference referenceVenue = FirebaseDatabase.getInstance().getReference("Venue");
 
+            if(currentTabPos == 0){
 
-            reference.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
-                       if(singleSnapshot.child("userNickname").getValue().toString().toLowerCase().contains(keyword)){
-                           mUserList.add(singleSnapshot.child("userNickname").getValue().toString());
+                referenceUser.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String[] key;
+
+                        for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                            key = singleSnapshot.child("userInterests").getValue().toString().toLowerCase().split(",");
+                            for (int index = 0; index < key.length; index++){
+                                if(key[index].contains(keyword)){
+                                    mUserList.add(singleSnapshot.child("userNickname").getValue().toString());
+                                    break;
+                                }
+                            }
                         }
-                       // mUserList.add(singleSnapshot.child("userInterests").getValue().toString().toLowerCase());
+                        updateUserName();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
 
                     }
-                    updateUserName();
-                }
+                });
+                referenceEvent.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String[] key;
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
+                        for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                            key = singleSnapshot.child("Event Tags").getValue().toString().toLowerCase().split(",");
+                            for (int index = 0; index < key.length; index++){
+                                if(key[index].contains(keyword)){
+                                    mUserList.add(singleSnapshot.child("Event Name").getValue().toString());
+                                    break;
+                                }
+                            }
+                        }
+                        updateUserName();
+                    }
 
-                }
-            });
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+                referenceVenue.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String[] key;
+
+                        for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                            key = singleSnapshot.child("VenueTag").getValue().toString().toLowerCase().split(",");
+                            for (int index = 0; index < key.length; index++){
+                                if(key[index].contains(keyword)){
+                                    mUserList.add(singleSnapshot.child("VenueName").getValue().toString());
+                                    break;
+                                }
+                            }
+                        }
+                        updateUserName();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+            }
+            if(currentTabPos == 1){
+                referenceUser.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                            if(singleSnapshot.child("userNickname").getValue().toString().toLowerCase().contains(keyword)){
+                                mUserList.add(singleSnapshot.child("userNickname").getValue().toString());
+                            }
+                            else if(singleSnapshot.child("userFullName").getValue().toString().toLowerCase().contains(keyword)){
+                                mUserList.add(singleSnapshot.child("userNickname").getValue().toString());
+                            }
+                        }
+                        updateUserName();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+            if(currentTabPos == 2){
+                referenceEvent.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                            if(singleSnapshot.child("Event Name").getValue().toString().toLowerCase().contains(keyword)){
+                                mUserList.add(singleSnapshot.child("Event Name").getValue().toString());
+                            }
+                        }
+                        updateUserName();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+            if(currentTabPos == 3){
+                referenceVenue.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                            if(singleSnapshot.child("VenueName").getValue().toString().toLowerCase().contains(keyword)){
+                                mUserList.add(singleSnapshot.child("VenueName").getValue().toString());
+                            }
+                        }
+                        updateUserName();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
         }
     }
+
     private void initTextListener(){
         mUserList =  new ArrayList<>();
         mSearchParam.addTextChangedListener(new TextWatcher() {
@@ -132,7 +257,24 @@ public class SearchTab extends AppCompatActivity {
     }
 
     private void updateUserName(){
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mUserList);
-        mListView.setAdapter(arrayAdapter);
+        LinkedHashSet<String> lhSetColors = new LinkedHashSet<String>(mUserList);
+        String[] newArray = lhSetColors.toArray(new String[ lhSetColors.size() ]);
+        List<String> list = Arrays.asList(newArray);
+        CustomListAdapter listAdapter = new CustomListAdapter(this , R.layout.custom_list , list);
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mUserList);
+        mListView.setAdapter(listAdapter);
+    }
+
+    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+
+        Toast.makeText(SearchTab.this,"pos"+position,Toast.LENGTH_LONG).show();
+
+        // Then you start a new Activity via Intent
+//        Intent intent = new Intent();
+//        intent.setClass(this, ListItemDetail.class);
+//        intent.putExtra("position", position);
+//        // Or / And
+//        intent.putExtra("id", id);
+//        startActivity(intent);
     }
 }
